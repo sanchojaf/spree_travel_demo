fce95c9d131b2b5faf9128a5c518566b9eb65094
Pedro Quintero Rojas <pqr@uh.cu>
2013-07-25 14:48:40 -0400

    primer commit del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CubanSwing::Application.config.secret_token = '72852e8715c01df60f401391b622b125b496a16dd3d95646c55a8c7485b9dc6d1e23482ec77cd938fb9677c817ffc4df0f9169c68abe1e0ebe3479268ef5ed4a'
