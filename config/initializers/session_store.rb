95a5dfe795bf81c4bc527a04a9bf8b27dfa6cfd0
snc <snc@jaf.com>
2013-08-16 11:11:52 -0500

    modified session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-CubanSwing::Application.config.session_store :cookie_store, :key => '_cuban_swing_session'
+SpreeTravelDemo::Application.config.session_store :cookie_store, :key => '_spree_travel_demo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# CubanSwing::Application.config.session_store :active_record_store
+# SpreeTravelDemo::Application.config.session_store :active_record_store
